434c263efe7a8e466cc2b6cab37d4088467d1117
Benjamin Althues <benjamin@babab.nl>
2015-07-25 00:23:29 +0200

    Add tappable classes

--- js/main.js
+++ js/main.js
@@ -5,17 +5,39 @@ var app = {
         var self = this;
 
         this.store = new LocalStorageStore(function() {
-            self.showAlert('Info', 'Welcome to Kebokun App');
+            self.showAlert('Welcome to Kebokun App', 'Info');
             $('body').html(new HomeView(self.store).render().el);
         });
+
+        this.registerEvents();
     },
 
-    showAlert: function (title, message) {
+    showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         }
         else {
             alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    registerEvents: function() {
+        var self = this;
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+        else {
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
         }
     }
 };
