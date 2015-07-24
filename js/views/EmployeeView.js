fd6f0a30bc8429e5c28baf9cfd00d5c2ae7fdcd8
Benjamin Althues <benjamin@babab.nl>
2015-07-25 00:47:23 +0200

    Add EmployeeView

---
+++ js/views/EmployeeView.js
@@ -0,0 +1,13 @@
+var EmployeeView = function(store) {
+    this.initialize = function() {
+        this.el = $('<div/>');
+    };
+    this.initialize();
+
+    this.render = function() {
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+}
+
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
