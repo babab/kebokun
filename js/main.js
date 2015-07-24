809d99fa08c91cedc70674462bc8c192e38f02a0
Benjamin Althues <benjamin@babab.nl>
2015-07-24 23:43:46 +0200

    Separate view and add styling

--- js/main.js
+++ js/main.js
@@ -1,33 +1,23 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
 
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($('#home-tpl').html());
-        this.employeeLiTpl = Handlebars.compile($('#employee-li-tpl').html());
+
         this.store = new LocalStorageStore(function() {
-            self.renderHomeView();
+            self.showAlert('Hi there', 'Welcome to Kebokun App');
+            $('body').html(new HomeView(self.store).render().el);
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
-    showAlert: function (message, title) {
+    showAlert: function (title, message) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         }
         else {
             alert(title ? (title + ": " + message) : message);
         }
-    },
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 };
 
