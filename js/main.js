faf912eb1068d168707b63ec9e4c2dea0a000645
Benjamin Althues <benjamin@babab.nl>
2015-07-24 21:39:04 +0200

    Add alert logic

--- js/main.js
+++ js/main.js
@@ -14,10 +14,21 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
+        var self = this;
+        this.store = new LocalStorageStore(function() {
+            self.showAler('Store initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        }
+        else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    }
 };
 
 app.initialize();
